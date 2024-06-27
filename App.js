73a68d991614b0b1c3c55503182d8d0c3b98a0b2
Jagannathbishwal <131639281+Jagannathbishwal@users.noreply.github.com>
2024-06-27 16:21:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import './App.css';
+import EmpCreate from './EmpCreate';
+import EmpListing from './EmpListing';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React JS CRUD operations</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<EmpListing/>}></Route>
+        <Route path='/employee/create' element={<EmpCreate/>}></Route>
+        <Route path='/employee/detail/:empid' element={<EmpDetail/>}></Route>
+        <Route path='/employee/edit/:empid' element={<EmpEdit/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
